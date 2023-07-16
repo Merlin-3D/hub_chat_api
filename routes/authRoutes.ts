f5c3aa9e510bda2fa36ee2e09fa5eceb53f8205d
Merlin-3D <axel.deffo@gmail.com>
2023-07-16 16:01:26 +0100

    Revert "first commit"
    This reverts commit ba28c745726380caf299e398972cae3e0404f172.

--- routes/authRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { login, signup } from '../controller/authController';
-
-const authRouter = Router();
-
-authRouter.post('/auth/signup', signup);
-authRouter.post('/auth/login', login);
-
-export default authRouter;
